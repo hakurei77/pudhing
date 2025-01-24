4ba94b0682f77b3d98b99a8d1793375dd73d5630
hakurei77 <hakurei77@foxmail.com>
2025-01-24 16:16:18 +0800

    添加了tailwindcss，删除了一些无关的文件

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('lint-staged').Config}  */
-export default {
-    '*.{js,ts,jsx,tsx,vue}': [
-        'eslint --fix',
-    ],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import path from "path";
 //引入svg需要用到的插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+//引入tailwindcss
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
     return {
         plugins: [vue(),
+            tailwindcss(),
             createSvgIconsPlugin({
                 iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
                 symbolId: 'icon-[dir]-[name]'

07a957d0ba44559459dcadce3096912a31bf90c9
hakurei77 <hakurei77@foxmail.com>
2025-02-26 23:59:52 +0800

    🐛 fix：修改了后端的一些bug

---
+++ apps/pudhingLLM-server/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        name: "pudhing",
+        script: "pnpm",
+        args: "dev",
+        cwd: "/www/wwwroot/hakurei77-pudhing-server",
+        env: {
+            NODE_ENV: "production",
+        },
+    },
+];
